c329edb0d2867c489510eedba8490bab92777d11
bogdan <toqwer00@outlook.com>
2025-03-13 14:21:27 +0300

    Изменил структуру проекта,применил подход Object-Oriented для создания окна

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,111 @@
-#ifndef UNICODE 
-#define UNICODE 
+#ifndef UNICODE
+#define UNICODE
 #endif
 
 #include <Windows.h>
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam,
-LPARAM lParam);
+template <class DERIVED_TYPE>
+class BaseWindow {
+public:
+    static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+        DERIVED_TYPE* pThis = NULL;
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-PWSTR pCmdLine, int nCmdShow) {
-    const wchar_t CLASS_NAME[] = L"TrawshiPlanner";
+        if (uMsg == WM_NCCREATE) {
+            CREATESTRUCT* pCreate = (CREATESTRUCT*)lParam;
+            pThis = (DERIVED_TYPE*)pCreate->lpCreateParams;
+            SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)pThis);
 
-    WNDCLASS wc = {};
-    wc.lpfnWndProc = WindowProc; 
-    wc.hInstance = hInstance;
-    wc.lpszClassName = CLASS_NAME;
+            pThis->m_hwnd = hwnd;
+        } else {
+            pThis = (DERIVED_TYPE*)GetWindowLongPtr(hwnd, GWLP_USERDATA);
+        }
 
-    RegisterClass(&wc);
-
-    HWND hwnd = CreateWindowEx(0,
-    CLASS_NAME,
-    L"TrawshiPlanner",
-    WS_OVERLAPPEDWINDOW,
-
-    CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-    
-    NULL,
-    NULL,
-    hInstance,
-    NULL
-);
-
-if (hwnd == NULL) {
-    return 0;
-}
-
-ShowWindow(hwnd, nCmdShow);
-
-MSG msg = {};
-while(GetMessage(&msg, NULL,0,0)>0) {
-    TranslateMessage(&msg);
-    DispatchMessage(&msg);
-}
-return 0;
-}
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam,
-LPARAM lParam) {
-
-switch(uMsg) {
-    case WM_DESTROY:
-    PostQuitMessage(0);
-    return 0;
-
-    case WM_PAINT : {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
-
-        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW+1));
-        EndPaint(hwnd, &ps);
+        if (pThis) {
+            return pThis->HandleMessage(uMsg, wParam, lParam);
+        } else {
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+        }
     }
+
+    BaseWindow() : m_hwnd(NULL) {}
+
+    BOOL Create(
+        PCWSTR lpWindowName,
+        DWORD dwStyle,
+        DWORD dwExStyle = 0,
+        int x = CW_USEDEFAULT,
+        int y = CW_USEDEFAULT,
+        int nWidth = CW_USEDEFAULT,
+        int nHeight = CW_USEDEFAULT,
+        HWND hWndParent = 0,
+        HMENU hMenu = 0
+    ) {
+        WNDCLASS wc = {0};
+
+        wc.lpfnWndProc = DERIVED_TYPE::WindowProc;
+        wc.hInstance = GetModuleHandle(NULL);
+        wc.lpszClassName = ClassName();
+
+        if (!RegisterClass(&wc)) {
+            return FALSE;
+        }
+
+        m_hwnd = CreateWindowEx(
+            dwExStyle, ClassName(), lpWindowName, dwStyle, x, y,
+            nWidth, nHeight, hWndParent, hMenu, GetModuleHandle(NULL),
+            this
+        );
+
+        return (m_hwnd ? TRUE : FALSE);
+    }
+
+    HWND Window() const { return m_hwnd; }
+
+protected:
+    virtual PCWSTR ClassName() const = 0;
+    virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) = 0;
+
+    HWND m_hwnd;
+};
+
+class MainWindow : public BaseWindow<MainWindow> {
+public:
+    PCWSTR ClassName() const { return L"TrawshiPlanner"; }
+
+    LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) {
+        switch (uMsg) {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+
+        case WM_PAINT: {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(m_hwnd, &ps);
+            FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+            EndPaint(m_hwnd, &ps);
+        }
+        return 0;
+
+        default:
+            return DefWindowProc(m_hwnd, uMsg, wParam, lParam);
+        }
+    }
+};
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow) {
+    MainWindow win;
+
+    if (!win.Create(L"TrawshiPlanner", WS_OVERLAPPEDWINDOW)) {
+        return 0;
+    }
+
+    ShowWindow(win.Window(), nCmdShow);
+
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
     return 0;
-}
-return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
